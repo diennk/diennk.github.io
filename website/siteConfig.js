8cea4aa21cd8f1a42cd3ae08b5c46492c5fc1540
Dien Nguyen <khanhdien@gmail.com>
2020-06-19 22:49:41 +0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Diens Notes', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://diennk.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/diennk.github.io/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
