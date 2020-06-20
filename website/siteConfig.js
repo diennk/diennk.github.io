c98f11a2a7dc9ef1c2941e19e85532c5cd0ec849
Dien Nguyen <khanhdien@gmail.com>
2020-06-20 10:45:31 +0700

    favicon

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -48,8 +48,8 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
+  headerIcon: 'img/favicon-96x96.png',
+  footerIcon: 'img/favicon-96x96.png',
   favicon: 'img/favicon.ico',
 
   /* Colors for website */
